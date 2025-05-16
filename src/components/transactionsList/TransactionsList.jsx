572f19bae968f2fe0029533bc2a1444514bce2f1
MykolaLukianovych <lukjanovych@gmail.com>
2025-05-16 22:06:03 +0300

    transactionList-mobile

--- src/components/transactionsList/TransactionsList.jsx
+++ src/components/transactionsList/TransactionsList.jsx
@@ -1,60 +1,55 @@
-import TransactionsItem from '../transactionsItem/TransactionsItem'
-import s from "./TransactionsList.module.css"
+import TransactionsItem from "../transactionsItem/TransactionsItem";
+import s from "./TransactionsList.module.css";
+
+const data = [
+  {
+    id: 1,
+    date: "16.05.2025",
+    type: "-",
+    category: "other",
+    comment: "Gift for your wife",
+    sum: "300",
+  },
+  {
+    id: 2,
+    date: "16.05.2025",
+    type: "+",
+    category: "other",
+    comment: "Car",
+    sum: "10000",
+  },
+  {
+    id: 3,
+    date: "16.05.2025",
+    type: "-",
+    category: "other",
+    comment: "Me",
+    sum: "2000",
+  },
+  {
+    id: 4,
+    date: "16.05.2025",
+    type: "+",
+    category: "other",
+    comment: "Mother",
+    sum: "1000",
+  },
+];
 
 const TransactionsList = () => {
-  const data = [
-    {
-      id: 1,
-      date: '16.05.2025',
-      type: '-',
-      category: 'other',
-      comment: 'Gift for your wife',
-      sum: '300',
-    },
-    {
-      id: 2,
-      date: '16.05.2025',
-      type: '-',
-      category: 'other',
-      comment: 'Car',
-      sum: '10000',
-    },
-    {
-      id: 3,
-      date: '16.05.2025',
-      type: '-',
-      category: 'other',
-      comment: 'Me',
-      sum: '2000',
-    },
-    {
-      id: 4,
-      date: '16.05.2025',
-      type: '-',
-      category: 'other',
-      comment: 'Mother',
-      sum: '1000',
-    },
-  ];
-  console.log("Компонент рендериться");
-  return (
-    <div className={s.listWrapper}>
-      <div className={s.header}>
-        <span>Date</span>
-        <span>Type</span>
-        <span>Category</span>
-        <span>Comment</span>
-        <span>Sum</span>
-        <span>Actions</span>
-      </div>
+  if (!data.length) {
+    return <p className={s.placeholder}>No transactions yet</p>;
+  }
 
-      <div className={s.items}>
+  return (
+    <div className={s.scrollWrapper}>
+      <ul className={s.list}>
         {data.map((item) => (
-        <TransactionsItem key={item.id} {...item} />
+          <TransactionsItem key={item.id} {...item} />
         ))}
-      </div>
+      </ul>
     </div>
   );
 };
 
-export default TransactionsList
+export default TransactionsList;
