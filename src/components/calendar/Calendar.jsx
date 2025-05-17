7bbaa0fbebe895db59510262fb62abc1b5bfe066
Ivan Shevtsov <vanyashevcov@ukr.net>
2025-05-17 12:08:37 +0300

    Create calendar

--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import { forwardRef, useState } from "react";
+import DatePicker from "react-datepicker";
+import { enUS } from "date-fns/locale";
+import { registerLocale } from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import s from "./Calendar.module.css";
+
+const CustomInput = forwardRef(function Calendar({ value, onClick }, ref) {
+  return (
+    <button className={s.customDateInput} onClick={onClick} ref={ref}>
+      <span>{value}</span>
+      <svg className={s.icon}>
+        <use href="/sprite.svg#icon-calendar" />
+      </svg>
+    </button>
+  );
+});
+
+const customLocale = {
+  ...enUS,
+  options: {
+    ...enUS.options,
+    weekStartsOn: 1,
+  },
+};
+
+registerLocale("custom-en", customLocale);
+
 
 const Calendar = () => {
+  const [startDate, setStartDate] = useState(new Date());
   return (
     <div>
-      
+      <DatePicker
+        locale="custom-en"
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
+        dateFormat="dd.MM.yyyy"
+        customInput={<CustomInput />}
+        weekStartsOn={1}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Calendar
+export default Calendar;
