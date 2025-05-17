47de41717fbf7dba858b9525bc8421502d9b28e9
Mariia Skydan <kyrosika565@gmail.com>
2025-05-17 14:02:10 +0300

    change eit transaction

--- src/components/editTransactionForm/EditTransactionForm.jsx
+++ src/components/editTransactionForm/EditTransactionForm.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Toggle from '../toggle/Toggle'
 import Calendar from '../calendar/Calendar'
 
 const EditTransactionForm = () => {
+  const [date, setDate] = useState();
   return (
     <div>
       <Toggle />
