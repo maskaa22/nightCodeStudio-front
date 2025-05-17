722174173f73aa9c08c66a3edb1e2bc24649bca2
Anna Bieliavtseva <belyavtseva1911@gmail.com>
2025-05-17 15:04:35 +0200

    add AddTransactionBtn and ModalAddTransaction

--- src/components/addTransactionForm/AddTransactionForm.jsx
+++ src/components/addTransactionForm/AddTransactionForm.jsx
@@ -1,12 +1,122 @@
-import React from "react";
-import Toggle from "../toggle/Toggle";
-import Calendar from "../calendar/Calendar";
+import Toggle from '../toggle/Toggle';
+// import Calendar from '../calendar/Calendar';
+import css from './AddTransactionForm.module.css';
+import { Field, Form, Formik } from 'formik';
+import Select from 'react-select';
+import { useState } from 'react';
 
-const AddTransactionForm = () => {
+const customStyles = {
+  control: (base, state) => ({
+    ...base,
+    backgroundColor: 'inherit',
+    borderColor: state.isFocused && state.isSelected ? '#355359' : '#1e2f33',
+    borderRadius: '8px',
+    boxShadow: 'none',
+    padding: '9px 0',
+    color: '#081222',
+    fontSize: '18px',
+    fontFamily: 'Inter',
+    fontWeight: '500',
+  }),
+  option: (base, state) => ({
+    ...base,
+    background: state.isSelected
+      ? 'linear-gradient(180deg, #355359 0%, #3b5d63 100%)'
+      : state.isFocused
+      ? 'linear-gradient(180deg, #355359 0%, #3b5d63 100%)'
+      : 'transparent',
+    color: '#FCFCFC',
+    fontSize: '16px',
+    padding: '12px 16px',
+    cursor: 'pointer',
+  }),
+  menu: (base) => ({
+    ...base,
+    backgroundColor: '#294045',
+    borderRadius: '8px',
+    overflow: 'hidden',
+    marginTop: 4,
+  }),
+  singleValue: (base) => ({
+    ...base,
+    color: '#081222',
+  }),
+  indicatorSeparator: () => ({
+    display: 'none',
+  }),
+  dropdownIndicator: (base) => ({
+    ...base,
+    color: '#081222',
+    padding: '0 8px',
+  }),
+  placeholder: (base) => ({
+    ...base,
+    color: '#081222',
+  }),
+};
+// i will take this options from backend
+const options = [
+  { value: 'main', label: 'Main expenses' },
+  { value: 'products', label: 'Products' },
+  { value: 'car', label: 'Car' },
+  { value: 'selfCare', label: 'Self care' },
+  { value: 'childCare', label: 'Child care' },
+  { value: 'household', label: 'Household products' },
+  { value: 'education', label: 'Education' },
+  { value: 'leisure', label: 'Leisure' },
+  { value: 'entertainment', label: 'Entertainment' },
+];
+
+const AddTransactionForm = ({onClose}) => {
+  const [isExpense, setIsExpense] = useState('false');
   return (
     <div>
-      <Toggle />
-      <Calendar />
+      <div className={css.toggle}>
+        <Toggle
+          onChange={(isExpense) =>
+            isExpense ? setIsExpense(true) : setIsExpense(false)
+          }
+        />
+      </div>
+      <div className={css.formWrapper}>
+        <Formik
+          initialValues={{ category: '', amount: '', comment: '' }}
+          onSubmit={() => {}}
+        >
+          <Form>
+            <div className={css.form}>
+              {isExpense && (
+                <Select
+                  options={options}
+                  styles={customStyles}
+                  placeholder="Category"
+                  isSearchable={false}
+                />
+              )}
+              <Field
+                type="number"
+                id="amount"
+                name="amount"
+                placeholder="0.00"
+                className={css.field}
+              ></Field>
+              <Field
+                id="comment"
+                name="comment"
+                placeholder="Comment"
+                className={css.field}
+              ></Field>
+            </div>
+            <div className={css.btnWrapper}>
+              <button type="submit" className={css.btnAdd}>
+                Add
+              </button>
+              <button className={css.btnCancel} onClick={onClose}>Cancel</button>
+            </div>
+          </Form>
+        </Formik>
+      </div>
+      {/* <Calendar /> */}
     </div>
   );
 };
--- src/components/modalAddTransaction/ModalAddTransaction.jsx
+++ src/components/modalAddTransaction/ModalAddTransaction.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
-import AddTransactionForm from '../addTransactionForm/AddTransactionForm'
+import { useEffect } from 'react';
+import AddTransactionForm from '../addTransactionForm/AddTransactionForm';
+import css from './ModalAddTransaction.module.css';
 
-const ModalAddTransaction = () => {
+const ModalAddTransaction = ({ isOpen, onClose }) => {
+  useEffect(() => {
+    const handleEsc = (e) => {
+      if (e.key === 'Escape') {
+        onClose();
+      }
+    };
+
+    document.addEventListener('keydown', handleEsc);
+    return () => document.removeEventListener('keydown', handleEsc);
+  }, [onClose]);
+
+  if (!isOpen) return null;
   return (
-    <div>
-     
-      <AddTransactionForm/>
+    <div className={css.overlay} onClick={onClose}>
+      <div className={css.modal} onClick={(e) => e.stopPropagation()}>
+        <h2 className={css.title}>Add transaction</h2>
+        <button className={css.closeBtn} onClick={onClose}>
+          <svg className={css.icon}>
+            <use href="/sprite.svg#icon-close" />
+          </svg>
+        </button>
+        <AddTransactionForm onClose={onClose}/>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ModalAddTransaction
+export default ModalAddTransaction;
