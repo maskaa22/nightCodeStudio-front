4a0c2107c1eec2af3f4e8f7459519430f085cbf4
Yura Petryna <yur4ik911@gmail.com>
2025-05-18 12:02:43 +0300

    add autologin and yup

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,19 +1,29 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: "https://nightcodestudio-backend.onrender.com",
+  baseURL: 'https://nightcodestudio-backend.onrender.com',
 });
 
-
 export const registerThunk = createAsyncThunk(
-  "auth/register",
+  'auth/register',
   async (body, thunkAPI) => {
     try {
-      const { data } = await api.post("/auth/register", body);
+      const { data } = await api.post('/auth/register', body);
       return data;
     } catch (error) {
-      return thunkAPI.rejectWithValue(error.message);
+      return thunkAPI.rejectWithValue(error.response);
     }
-  }
+  },
+);
+export const loginThunk = createAsyncThunk(
+  'auth/login',
+  async (body, thunkAPI) => {
+    try {
+      const { data } = await api.post('/auth/login', body);
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response);
+    }
+  },
 );
