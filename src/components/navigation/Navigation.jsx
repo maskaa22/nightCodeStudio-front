38fb4e81a593243ed7383f027b9853af0650ceb4
SSiukh <siukhsasha@gmail.com>
2025-05-18 18:23:10 +0300

    navigation: fix absolute position

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,7 +1,7 @@
-import { NavLink } from "react-router-dom";
-import s from "./Navigation.module.css";
-import { getNavLinkClass } from "../../utils/utils";
-import { useMediaQuery } from "react-responsive";
+import { NavLink } from 'react-router-dom';
+import s from './Navigation.module.css';
+import { getNavLinkClass } from '../../utils/utils';
+import { useMediaQuery } from 'react-responsive';
 
 const Navigation = () => {
   const isMobile = useMediaQuery({ maxWidth: 767 });
@@ -10,9 +10,11 @@ const Navigation = () => {
     <div className={s.container}>
       <NavLink to="/" className={(props) => getNavLinkClass(props, s)}>
         <div className={s.linkContainer}>
-          <svg className={s.icon} width="27" height="24">
-            <use href="/sprite.svg#icon-home" />
-          </svg>
+          <div className={s.iconLayout}>
+            <svg className={s.icon} width="27" height="24">
+              <use href="/sprite.svg#icon-home" />
+            </svg>
+          </div>
           {!isMobile && <p className={s.linkText}>Home</p>}
         </div>
       </NavLink>
@@ -22,9 +24,11 @@ const Navigation = () => {
         className={(props) => getNavLinkClass(props, s)}
       >
         <div className={s.linkContainer}>
-          <svg className={s.icon} width="26" height="15">
-            <use href="/sprite.svg#icon-stats" />
-          </svg>
+          <div className={s.iconLayout}>
+            <svg className={s.icon} width="26" height="15">
+              <use href="/sprite.svg#icon-stats" />
+            </svg>
+          </div>
           {!isMobile && <p className={s.linkText}>Statistics</p>}
         </div>
       </NavLink>
@@ -33,9 +37,11 @@ const Navigation = () => {
           to="/currency"
           className={(props) => getNavLinkClass(props, s)}
         >
-          <svg className={s.icon} width="13" height="22">
-            <use href="/sprite.svg#icon-currency" />
-          </svg>
+          <div className={s.iconLayout}>
+            <svg className={s.icon} width="13" height="22">
+              <use href="/sprite.svg#icon-currency" />
+            </svg>
+          </div>
         </NavLink>
       )}
     </div>
