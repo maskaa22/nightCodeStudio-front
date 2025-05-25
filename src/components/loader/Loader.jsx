69b47dcc8df538d639b3fb8a0fb739ff9a0e5f1d
Psyche1 <vovchik357@gmail.com>
2025-05-25 22:39:51 +0300

    loader

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,6 +1,10 @@
+import { useSelector } from 'react-redux';
 import { BounceLoader } from 'react-spinners'
+import { selectIsLoading } from '../../redux/loader/selectors';
 
 const Loader = () => {
+  const isLoading = useSelector(selectIsLoading);
+  if (!isLoading) return null
   return (<BounceLoader color="#1e2f33" />  );
 };
 
